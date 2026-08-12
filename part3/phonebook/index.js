9f5f4d65c62e5842d89dc1d0232f006eb6557c75
Abraham Briones <abebriones98@gmail.com>
2026-08-12 17:05:15 -0400

    part3 update port

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // SHow data sent in HTTP Post request
 morgan.token('reqBody', (req, res) => JSON.stringify(req.body));
@@ -90,7 +92,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
